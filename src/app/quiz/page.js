82380c6e6a28c76e6930228c8f138c5e08cb4a4f
Joker <admin@e.mail.de>
2023-07-24 17:20:26 +0200

    Changed audio linking

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -13,7 +13,7 @@ import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-ki
 import Sortable from "./Sortable";
 
 export default function Page() {
-   const audio = new Audio("audio/5-sec-countdown.mp3");
+   const audio = new Audio("../../../public/audio/5-sec-countdown.mp3");
    const router = useRouter();
    const [data, setData] = useState([]);
    const [user, setUser] = useState({});
