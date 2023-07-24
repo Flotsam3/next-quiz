c9c46e288b6f22d0b653c0de91a4de4750dd5453
Joker <admin@e.mail.de>
2023-07-24 18:27:38 +0200

    Adjusted next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+   webpack: (config, { isServer }) => {
+      // Add a rule to handle MP3 files
+      config.module.rules.push({
+         test: /\.(mp3)$/,
+         use: {
+            loader: "file-loader",
+            options: {
+               publicPath: "/_next/",
+               name: "static/media/[name].[hash].[ext]",
+            },
+         },
+      });
+
+      // For server-side rendering of the audio file
+      if (!isServer) {
+         config.node = {
+            fs: "empty",
+         };
+      }
+
+      return config;
+   },
+};
 
 module.exports = nextConfig;
